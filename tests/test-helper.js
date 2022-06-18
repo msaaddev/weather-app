f505294c603d01d102d639fc222039bf4789cc41
msaaddev <mrsaadirfan@gmail.com>
2022-06-19 01:33:07 +0500

    📦 NEW: First commit

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'weather-app/app';
+import config from 'weather-app/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
