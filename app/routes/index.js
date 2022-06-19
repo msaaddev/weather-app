b50b06970ecca1a33e85b2c87f9c720c24402b5e
msaaddev <mrsaadirfan@gmail.com>
2022-06-19 19:35:14 +0500

    👌 IMPROVE: Manipulated API response

--- app/routes/index.js
+++ app/routes/index.js
@@ -28,7 +28,19 @@ export default class IndexRoute extends Route {
       const response = await fetch(
         `${api}?lat=${latitude}&lon=${longitude}&appid=${ENV.APP.OPEN_WEATHER_MAP_API_KEY}&units=metric`
       );
-      const res = await response.json();
+      let res = await response.json();
+
+      // manipulate the response to make it more usable
+      let main = { ...res.main };
+      let weather = { ...res.weather[0] };
+      let description = weather.description;
+      description = description.charAt(0).toUpperCase() + description.slice(1);
+      weather = { ...weather, description };
+      weather = [weather];
+      const temp = Math.floor(main.temp);
+      main = { ...main, temp };
+      res = { ...res, main, weather };
+
       return res;
     } catch (err) {
       console.log(err);
